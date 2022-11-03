ebeeb432da088a7b63aabbd3e0efa13b58f48266
Andrii Shpak <SHPAKK2@GMAIL.COM>
2022-11-03 14:30:52 +0200

    1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,21 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems))
+
+function createGalleryItem(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join("")
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionPosition: "bottom", captionDelay: 250});
